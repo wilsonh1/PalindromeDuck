d367eb9ffd6e08cf93fdae4911802b14744c2e9d
wilsonh1 <35614603+wilsonh1@users.noreply.github.com>
2018-05-26 16:42:43 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  consumer_key: 'blah',
-  consumer_secret: 'blah',
-  access_token: 'blah',
-  access_token_secret: 'blah'
+  consumer_key: 'Qtznoyi1gZORNck9idX0ptniH',
+  consumer_secret: '4ue9milzXqO1ehMJcY1eXW1WdVC9JltiRUM5b5i1cxD8vxC0Fk',
+  access_token: '1000276587298803713-kRG0B3weoIzemwMSnDEOCHqJoI6aVO',
+  access_token_secret: 'px9Lq4v6DME79zZHPzJpUiXqAfKsF4eC3aVHnsVTrdaPS'
 }
