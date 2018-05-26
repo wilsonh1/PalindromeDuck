a9676683b136b0fd8a6c1296aa86d1f0d351d5df
wilsonh1 <35614603+wilsonh1@users.noreply.github.com>
2018-05-26 16:50:03 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,6 +7,10 @@ a.setTimezone("America/New_York");
 console.log(a.toString());
 */
 
+var Twit = require('twit')
+
+var T = new Twit(require('./config.js'))
+
 function checkPalindrome (s) {
 	len = s.length;
 	for (i=0; i<len/2; i++) {
@@ -29,15 +33,15 @@ console.log(hour+minutes);
 console.log(checkPalindrome(hour+minutes));
 */
 
-function ducker (time, date) {
+/*function ducker (time, date) {
 	if (time && date) 
 		return 'duck !!';
 	if (time)
 		return 'duck !';
 	return '';
-}
+}*/
 
-function checkTime () {
+function ducker () {
 	cur = new Date();
 
 	hour = cur.getHours().toString();
@@ -56,8 +60,24 @@ function checkTime () {
 	date = cur.getDate().toString();
 	year = cur.getFullYear().toString();
 	
-	console.log(ducker(checkPalindrome(hour+minutes), checkPalindrome(month+date+year)));
+	//console.log(ducker(checkPalindrome(hour+minutes), checkPalindrome(month+date+year)));
+	
+	time = checkPalindrome(hour+minutes);
+	date = checkPalindrome(month+date+year);
+	
+	if ((time && date) || checkPalindrome(hour+minutes+month+date+year)) 
+		T.post('statuses/update', { status: 'duck !!' }, tweeted)
+	else if (time) 
+		T.post('statuses/update', { status: 'duck !' }, tweeted)
+	
+}
+
+function tweeted (err, response) {
+	if (err !== undefined) 
+		console.log(err)
+	else 
+		console.log('Tweeted: ' + response)
 }
 
 //checkTime();
-setInterval(checkTime, 1000*60);
+setInterval(ducker, 1000*60);
