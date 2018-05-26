aa3428ad2fb5abac5a5be48c6172b82e5341ea18
wilsonh1 <35614603+wilsonh1@users.noreply.github.com>
2018-05-26 01:43:50 -0700

    Update test.js

--- test.js
+++ test.js
@@ -6,5 +6,5 @@ a.setTimezone("America/Los_Ange");
 console.log(a.toString());
 */
 
-cur = now.localtime(Date.now()/1000);
+cur = time.localtime(Date.now()/1000);
 console.log(cur['minutes'].toString());
