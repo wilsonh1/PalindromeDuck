478527790c24d91be295c9a66b560e2a94d7701a
wilsonh1 <35614603+wilsonh1@users.noreply.github.com>
2018-05-26 01:47:47 -0700

    Update test.js

--- test.js
+++ test.js
@@ -6,5 +6,12 @@ a.setTimezone("America/Los_Ange");
 console.log(a.toString());
 */
 
-cur = time.localtime(Date.now()/1000);
-console.log(cur['minutes'].toString());
+function checkPalindrome (curtime) {
+	if (curtime.length==3)
+		return Boolean(curtime[0]==curtime[2]);	
+	return Boolean(curtime[0]==curtime[3] && curtime[1]==curtime[2]);
+}
+
+var cur = time.localtime(Date.now()/1000);
+
+console.log(checkPalindrome(cur['hours'].toString()+cur['minutes'].toString()));
