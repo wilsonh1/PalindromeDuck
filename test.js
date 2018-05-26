9c9a5e4f7a69172bd83c4ed679cb4fcde07b3d79
wilsonh1 <35614603+wilsonh1@users.noreply.github.com>
2018-05-26 01:33:28 -0700

    Update test.js

--- test.js
+++ test.js
@@ -1,11 +1,8 @@
 var time = require ('time');
 
-var now = new time.Date();
+//var now = new time.Date();
 
-now.setTimezone("America/Los_Angeles");
+//now.setTimezone("America/Los_Angeles");
 
-cur = new time.Date();
-console.log(cur.toString());
-
-cur1 = time.localtime(Date.now()/1000);
-console.log(cur1['minutes']);
+cur = time.localtime(Date.now()/1000);
+console.log(typeof cur['minutes']);
