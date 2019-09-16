b6464601013687c0a3238b01938bae99f34688d7
Wilson Ho <wilson.ho2718@gmail.com>
2019-09-16 02:06:59 -0700

    Update config

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
 module.exports = {
-  consumer_key: 'Qtznoyi1gZORNck9idX0ptniH',
-  consumer_secret: '4ue9milzXqO1ehMJcY1eXW1WdVC9JltiRUM5b5i1cxD8vxC0Fk',
-  access_token: '1000276587298803713-kRG0B3weoIzemwMSnDEOCHqJoI6aVO',
-  access_token_secret: 'px9Lq4v6DME79zZHPzJpUiXqAfKsF4eC3aVHnsVTrdaPS'
-}
+  consumer_key: process.env.consumer_key,
+  consumer_secret: process.env.consumer_secret,
+  access_token: process.env.access_token,
+  access_token_secret: process.env.access_token_secret
